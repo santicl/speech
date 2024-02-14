9979839c95ea9cfe23e544e749225cf78318cfbe
santicl <caballerosuarezsantiago@gmail.com>
2024-02-14 17:59:36 -0500

    U files

--- src/App.js
+++ src/App.js
@@ -14,169 +14,43 @@ function App() {
       <section className="App-speech__content-speech">
 
         <div className="Content-speech">
-          <h1>Por qué podemos confiar en la promesa de la vida eterna</h1>
+          <h1>Busquemos perlas escondidas (10 mins)</h1>
+
+          <h3>Salmo 5: 9:</h3>
           <p>
-            <strong>¿Hasta dónde llegarían las personas para asegurar un futuro verdaderamente feliz y estable dentro de lo legal?</strong> La estabilidad laboral es común, pero <strong>¿qué pasa cuando las fuerzas para trabajar disminuyen?</strong> La pensión podría ser la solución.
+            Porque no se puede confiar en nada de lo que dicen;
+
+            solo hay malicia en su interior;
+
+            su garganta es una tumba abierta;
+
+            usan su lengua para halagar
+          </p>
+
+          <p><strong> 1) Pregunta:</strong> ¿Por qué dice la Biblia que la garganta de los malvados “es una tumba abierta”?</p>
+          <p>
+             <strong>Respuesta:</strong> Al igual que un sepulcro abierto ha de llenarse con muertos y con corrupción, su garganta se abre para hablar lo que es mortífero y corrupto
+          </p>
+
+          <p><strong> 2) Pregunta:</strong> ¿Qué perlas espirituales ha encontrado en la lectura bíblica de esta semana?</p>
+
+          <p><strong> Perla Prevencion:</strong></p>
+
+          <p>
+          En salmo capitulo 6 es interesante recordar la importancia de los títulos porque pueden identifican al escritor, suministran información histórica, proveen instrucciones musicales o indican el propósito del salmo.
           </p>
 
           <p>
-            Seamos honestos, nadie quiere ver disminuir sus fuerzas con los años. Este deseo surge porque fuimos diseñados para vivir eternamente, y se nos brindó la oportunidad de obtener vida eterna...
+          En este caso dice
+            <strong>"Para acompañarse con instrumentos de cuerda afinados en seminit"</strong>
           </p>
 
           <p>
-            <strong>¿Por qué confiar en la promesa de la vida eterna?</strong> Exploraremos tres razones.
+          Seminit son registros bajos... Es decir da instrucciones para que se acompañe con instrumentos de cuerda afinados con registros bajos...
           </p>
 
           <p>
-            La primera se encuentra en.
-          </p>
-
-          <h2>
-            Job 36: 26:
-          </h2>
-
-          <h2>26</h2> Sí, Dios es más grande de lo que podemos entender; el número de sus años escapa a nuestra comprensión.
-
-          <h3>La primera razón es que Jehová Dios es eterno...</h3>
-
-          <p>
-            Desde la óptica humana, “tan grande es Dios que no podemos verlo, ni saber cuántos años tiene”.
-          </p>
-
-          <p>
-            Recordemos <strong>Apocalipsis 1:8</strong>, que habla de Jehová como "el que es y que era y que viene..."
-          </p>
-
-          <p>
-            Centremonos en la parte... Que era...
-          </p>
-
-          <p>
-            Antes de que Jehová creara primeramente a Jesús y los demás ángeles... Jehová estaba solo y <strong>ERA</strong> completo en todo, no necesitaba crear nada para demostrar que <strong>ERA</strong> todopoderoso o Sabio.
-          </p>
-
-          <p>
-            Así que, después de haber creado a Jesús y los demás ángeles... Jehová le comunica a Jesús que haría al hombre a su imagen y semejanza...
-          </p>
-
-          <p>
-            No solo se refería a que nos crearía con cualidades semejantes a él... Sino con el deseo de vivir eternamente.
-          </p>
-
-          <p>
-            <strong>¿Qué sentido tendría esa promesa si él mismo no viviera para siempre?</strong>
-          </p>
-
-          <p>
-            Nadia puede dar algo de lo que no tiene...
-          </p>
-
-          <p>
-            <strong>Y lo eterno que es Jehová se vincula con la siguiente razón...</strong>
-          </p>
-
-          <p>Se encuentra en</p> <h2>Job 36: 27, 28:</h2>
-
-          <h2>27</h2> <p>Él hace subir las gotas de agua; el vapor se condensa para formar la lluvia;</p> <h2>28</h2> <p>entonces las nubes la derraman precipitándola sobre la humanidad.</p>
-          <p>
-            Como se manejan los procesos de la tierra, como el ciclo del agua... Demuestra que Jehová tiene la sabiduría y el poder necesarios para sostener la vida...
-          </p>
-
-
-          <p><strong>Y en el caso de la sabiduría... La eternidad de Jehová garantiza que él tiene sabiduría ilimitada... Ya que la biblia relaciona la edad con la sabiduría.</strong></p>
-
-          <p>
-            Veamos algo más interesante sobre la sabiduría de Jehová...
-          </p>
-
-          <h2>1 Pedro 1: 12 parte C:</h2>
-
-          <p>
-            <strong>Parte C</strong> Estas son las mismas cosas que los ángeles desean mirar con atención.
-          </p>
-
-          <p>
-            <strong>¿A qué cosas se refiere este versículo?</strong>
-          </p>
-
-          <p>
-            En como se desarrollaría el secreto sagrado aquí en la tierra...
-          </p>
-
-          <p>
-            Y cuando uno mira con atención algo es porque desea entender cómo y qué está pasando...
-          </p>
-
-          <p>
-            Los ángeles se benefician de observar cómo Jehová dirige a la congregación cristiana, pues así llegan a conocer mejor la sabiduría divina.
-          </p>
-
-          <p>
-            <strong>En pocas palabras, los ángeles aunque son más sabios e inteligentes que nosotros, ni siquiera ellos pueden llegar a entender al 100% la sabiduría de Jehová.</strong>
-          </p>
-
-          <p>
-            ¡Por eso confían en él y le sirven con tanta admiración!
-          </p>
-
-          <p>
-            Veamos un poco la imágen que está en la guía...
-          </p>
-
-          <img src={Hermana} />
-
-          <p>
-            Se ven felices a todos los que están en esa ilustración... Puede que a una persona la salte la siguiente duda... <strong>¿Si viviremos miles de años, será eso algo emocionante?</strong>
-          </p>
-
-          <p>
-            Pensemos nuevamente en el ejemplo de los ángeles... Llevan mucho más de 13 mil millones de años y aún en la actualidad siguen aprendiendo sobre la personalidad de Jehová...
-          </p>
-
-          <h2>
-            Juan 17: 3:
-          </h2>
-
-          <h2>3</h2> <p>Esto significa vida eterna: que lleguen a conocerte a ti, el único Dios verdadero, y a quien tú enviaste, Jesucristo.</p>
-
-          <p>
-            Todo ser humano puede tener vida eterna conociendo a Jehová y a su hijo Jesús... Así que la tercera razón es que...
-          </p>
-
-          <p>
-            Jehová nos enseña qué debemos hacer para conseguir vida eterna...
-          </p>
-
-          <p>
-            Bueno, ya sabemos 3 razones muy importantes que nos ayudan a confiar en la promesa de la vida eterna...
-          </p>
-
-          <p>
-            Pero ahora...
-          </p>
-
-
-          <h3>¿Como nos ayuda en nuestra vida actual tener esta promesa, de esta esperanza?</h3>
-
-
-          <h2>Hebreos 6: 19:</h2>
-
-          <h2>19</h2> <p>Esta esperanza segura y firme que tenemos es como un ancla para el alma y entra al lugar que está al otro lado de la cortina.</p>
-
-          <p>Posiblemente Pablo pensaba en un barco, porque unas de las cosas que hace un ancla es dar equilibrio a un barco...</p>
-
-          <p>
-            La esperanza es como un ancla porque nos ayuda a hacer frente a las pruebas, la persecución e incluso la muerte.
-          </p>
-
-          <p>Así que hermanos y personas que nos visitan...</p>
-
-          <p>
-            Confiamos en la promesa de la vida eterna por 3 razones demasiado importante... porque nos la promete alguien que es eterno, que tiene la sabiduría y poder ilimitados y que Jehová nos enseña lo que debemos hacer para conseguir la vida eterna. Además que nos ayuda afrontar dificultades y problemas...
-          </p>
-
-          <p>
-            Si confiamos plenamente en esta promesa de la vida eterna... Obtendremos la vida que en verdad es vida.
+          Es por eso que... Si queremos tener más entendimiento de las escrituras , nunca debemos pasar por alto los títulos.
           </p>
 
         </div>
