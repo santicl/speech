b2efec8df9fbddbf2b255eadc120f20430f109d9
santicl <caballerosuarezsantiago@gmail.com>
2024-03-20 14:53:37 -0500

    U files

--- src/App.js
+++ src/App.js
@@ -14,44 +14,83 @@ function App() {
       <section className="App-speech__content-speech">
 
         <div className="Content-speech">
-          <h1>Busquemos perlas escondidas (10 mins)</h1>
-
-          <h3>Salmo 5: 9:</h3>
+          <h1>¿Por qué los testigos de Jehová celebran la Cena del Señor de forma diferente a otras religiones? (5 mins)</h1>
           <p>
-            Porque no se puede confiar en nada de lo que dicen;
-
-            solo hay malicia en su interior;
-
-            su garganta es una tumba abierta;
-
-            usan su lengua para halagar
-          </p>
-
-          <p><strong> 1) Pregunta:</strong> ¿Por qué dice la Biblia que la garganta de los malvados “es una tumba abierta”?</p>
-          <p>
-             <strong>Respuesta:</strong> Al igual que un sepulcro abierto ha de llenarse con muertos y con corrupción, su garganta se abre para hablar lo que es mortífero y corrupto
-          </p>
-
-          <p><strong> 2) Pregunta:</strong> ¿Qué perlas espirituales ha encontrado en la lectura bíblica de esta semana?</p>
-
-          <p><strong> Perla Prevencion:</strong></p>
-
-          <p>
-          En salmo capitulo 6 es interesante recordar la importancia de los títulos porque pueden identifican al escritor, suministran información histórica, proveen instrucciones musicales o indican el propósito del salmo.
+          Si nos hicieran está pregunta <strong>¿Podríamos explicarle con base el porqué celebramos la cena del señor diferente a las otras religiones?</strong>
           </p>
 
           <p>
-          En este caso dice
-            <strong>"Para acompañarse con instrumentos de cuerda afinados en seminit"</strong>
+          Por eso, durante este discurso le daremos respuesta a esta pregunta... Y para ello estaremos viendo solo 2 puntos.
+          </p>
+
+          <ol>
+            <li>¿Por qué se celebra?.</li>
+            <li>El pan y el vino.</li>
+          </ol>
+
+          <h2>1) ¿Por qué se celebra?</h2>
+
+          <p>
+          Muchas religiones usan esta semana como si fuera algo especial para perdonar los pecados. Es decir, que durante todo el año pueden practicar lo que quieran... Pero en semana santa reciben perdón.
+          </p>
+
+          <h3>1 Corintios 11: 24:</h3>
+
+          <p>
+          <strong>24</strong> y, después de darle gracias a Dios, lo partió y dijo: “Esto representa mi cuerpo, que es dado en beneficio de ustedes. Sigan haciendo esto en memoria de mí”.
           </p>
 
           <p>
-          Seminit son registros bajos... Es decir da instrucciones para que se acompañe con instrumentos de cuerda afinados con registros bajos...
+          Nosotros Celebramos la Cena del Señor para recordar el sacrificio que Jesús hizo por nosotros y mostrar nuestro agradecimiento por haberlo hecho.
           </p>
 
           <p>
-          Es por eso que... Si queremos tener más entendimiento de las escrituras , nunca debemos pasar por alto los títulos.
+          Además, Dios puede perdonar nuestros pecados solo si ejercemos fe en Jesús y si nos arrepentimos de corazón.
           </p>
+
+          <h2>2) El pan y el vino.</h2>
+
+          <p>
+          En esta celebración que instituyó Jesús, usó el pan sin levadura y el vino tinto que habían sobrado de la comida de la Pascua.
+          </p>
+
+          <p>
+          Pero veamos que dice la biblia...
+          </p>
+
+          <h3>1 Corintios 5: 8:</h3>
+
+          <p>
+          <strong>8</strong> Por consiguiente, no celebremos la fiesta con la levadura vieja ni con la levadura de la maldad y la perversidad, sino con los panes sin levadura de la sinceridad y la verdad.
+          </p>
+
+          <p><strong>¿Que connotación tiene la levadura en este versículo?</strong></p>
+
+          <p>
+          Como algo negativo, hasta la compara con la maldad y la perversidad, y en otros versículos, representa el pecado y corrupción.
+          </p>
+
+          <p>
+          Y en el caso del vino, la biblia menciona vino, no jugó de uva...
+          </p>
+
+          <p>
+          Pero puede que nos pregunten <strong>¿Si en el pan se prohíbe levadura para que no lo fermente, porque se permite el vino si es fermentado?</strong>
+          </p>
+
+          <p>
+          En el caso del pan, está prohibición divina solo se refiere al pan o masa, en ningún momento al vino.
+          </p>
+
+          <p>
+          Otra razón importante es que el proceso de fermentación del pan es porque le añaden levadura, tiene intervención de una persona.
+          </p>
+
+          <p>
+          A diferencia del vino, la fermentación es natural, porque la levadura está presente en la piel de la uva y todo el proceso pasa de manera natural sin intervención de una persona.
+          </p>
+
+          <p><strong>En conclusión hermanos, nosotros seguimos está celebración tal cual aparece en la biblia, no le modificamos nada, vimos 2 razones de peso que aseguran que como la celebramos es como Jehová quiere.</strong></p>
 
         </div>
 
