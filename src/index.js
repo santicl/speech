c2de4c357f2a9d37c792250096bcb9d6b0eb9144
santicl <caballerosuarezsantiago@gmail.com>
2025-12-06 15:19:57 -0500

    Bosquejo 101

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import DiscourseTrueLove from './Love';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <DiscourseTrueLove />
+    <SpeechOutline />
   </React.StrictMode>
 );
 
